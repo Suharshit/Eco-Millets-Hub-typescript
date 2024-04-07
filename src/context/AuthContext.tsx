458ba8b5f83a5112f9b5e0cd639fe29b890f32cf
{suharshit} <{suharshit123@gmail.com}>
2024-04-07 17:52:44 +0530

    build added

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,34 +1,34 @@
-import { createContext, useState } from 'react';
-import authservice from '../appwrite/auth';
+// import { createContext, useState } from 'react';
+// import authservice from '../appwrite/auth';
 
-const INITIAL_STATE = {
-  setIsAuthenticated: () => {},
-  checkAuthUser: async () => false as boolean,
-};
+// // const INITIAL_STATE = {
+// //   setIsAuthenticated: () => {},
+// //   checkAuthUser: async () => false as boolean,
+// // };
 
-export interface AuthContextProps {
-    value: boolean;
-    setValue: (newValue: boolean) => void;
-  }
+// export interface AuthContextProps {
+//     value: boolean;
+//     setValue: (newValue: boolean) => void;
+//   }
 
-export const AuthContext = createContext<AuthContextProps>({
-    value: false,
-    setValue: () => {},
-  });
+// export const AuthContext = createContext<AuthContextProps>({
+//     value: false,
+//     setValue: () => {},
+//   });
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+// export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  const checkAuthUser = async() => {
-    try {
-      const currentAccount = await authservice.getCurrentUser();
-      if(currentAccount){
-        setIsAuthenticated(!isAuthenticated);
-        return true;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-  }
-}
+//   const checkAuthUser = async() => {
+//     try {
+//       const currentAccount = await authservice.getCurrentUser();
+//       if(currentAccount){
+//         setIsAuthenticated(!isAuthenticated);
+//         return true;
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+//   }
+// }
